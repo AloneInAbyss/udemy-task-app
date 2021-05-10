780b79f56d0f44008759e2e80931a31189f4cf7d
Thiago Assi <thiago-assi@outlook.com>
2021-05-10 09:52:26 -0300

    code cleaned

--- mongodb.js
+++ mongodb.js
@@ -1,20 +1,9 @@
 // CRUD - Cread, Read, Update, Delete
 
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
-
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
-
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-console.log(id.id)
-console.log(id.id.length)
-console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
   // If an error occurs
@@ -24,46 +13,6 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
   const db = client.db(databaseName)
 
-  // db.collection('users').insertOne({
-  //   name: 'Thiago',
-  //   age: 20
-  // }, (error, result) => {
-  //   // If an error occurs
-  //   if (error) {
-  //     return console.log('Unable to insert user')
-  //   }
-
-  //   console.log(result.ops)
-  // })
   
-  // db.collection('users').insertMany([
-  //   {
-  //     name: 'Jean',
-  //     age: 28
-  //   }, {
-  //     name: 'Barbara',
-  //     age: 20
-  //   }
-  // ], (error, result) => {
-  //   // If an error occurs
-  //   if (error) {
-  //     return console.log('Unable to insert users')
-  //   }
-
-  //   console.log(result.ops)
-  // })
-
-  // db.collection('tasks').insertMany([
-  //   {
-  //     description: 'study',
-  //     completed: false
-  //   }
-  // ], (error, result) => {
-  //   if (error) {
-  //     return console.log('Unable to insert tasks')
-  //   }
-
-  //   console.log(result.ops)
-  // })
 
 })
