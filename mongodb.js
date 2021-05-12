88ea005691d5aa866d9139cfc00907546f07ec2f
Thiago Assi <thiago-assi@outlook.com>
2021-05-12 07:23:11 -0300

    Update One

--- mongodb.js
+++ mongodb.js
@@ -13,6 +13,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
   const db = client.db(databaseName)
 
-  
+  db.collection('users').updateOne({
+    _id: new ObjectID("6098dd1ea0f3ec2c2887be40")
+  }, {
+    $set: {
+      name: 'Diluc'
+    }
+  }).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.log(error);
+  })
 
 })
