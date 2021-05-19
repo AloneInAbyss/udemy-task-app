cf973dc5b240dff1103945bed0d89d497b8ac262
Thiago Assi <thiago-assi@outlook.com>
2021-05-19 05:57:37 -0300

    resource creation endpoints part II

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -4,15 +4,3 @@ mongoose.connect(
   'mongodb://localhost:27017/task-manager', 
   { useNewUrlParser: true, useUnifiedTopology: true }
 )
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-})
