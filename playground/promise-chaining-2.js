0c346523cffec596c0969b70bb049058f8842760
Thiago Assi <thiago-assi@outlook.com>
2021-05-19 15:05:58 -0300

    async await challenge

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,16 @@
 require('../src/database/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('609bbfd77ed18e2fe06eb152').then((task) => {
-  console.log(task);
-  return Task.countDocuments({ completed: false })
-}).then((result) => {
-  console.log(result);
+deleteTaskAndCountCompleted = async (id) => {
+  const task = await Task.findByIdAndDelete(id)
+  console.log(task)
+  const result = await Task.countDocuments({ completed: false })
+  // console.log(result)
+  return result
+}
+
+deleteTaskAndCountCompleted('60a5529b99888a0b149af897').then((result) => {
+  console.log(result)
 }).catch((e) => {
-  console.log(e);
-}) 
+  console.log(e)
+})
