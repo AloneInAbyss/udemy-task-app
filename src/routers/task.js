d7b9c1ec2f6d98d42d0fc03e17027751a0031899
Thiago Assi <thiago-assi@outlook.com>
2021-05-31 19:25:34 -0300

    users and tasks relationship

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
+
+router.post('/tasks', auth, async (req, res) => {
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id
+  })
+
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
 
 router.get('/tasks', async (req, res) => {
   try {
@@ -22,17 +37,6 @@ router.get('/tasks/:id', async (req, res) => {
     res.send(task)
   } catch (e) {
     res.status(500).send(e)
-  }
-})
-
-router.post('/tasks', async (req, res) => {
-  const task = new Task(req.body)
-
-  try {
-    await task.save()
-    res.status(201).send(task)
-  } catch (e) {
-    res.status(400).send(e)
   }
 })
 
