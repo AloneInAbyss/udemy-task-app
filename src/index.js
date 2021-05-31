14bba77559b7c21a03c40ecdda7a37a3172cbcd4
Thiago Assi <thiago-assi@outlook.com>
2021-05-31 20:08:05 -0300

    cascade delete tasks

--- src/index.js
+++ src/index.js
@@ -13,19 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-  // const task = await Task.findById('60b55eeaf8e1913dd8db0528')
-  // // Find the user associated with this task
-  // await task.populate('owner').execPopulate()
-  // console.log(task.owner)
-
-  const user = await User.findById('60b55eb1f8e1913dd8db0525')
-  await user.populate('tasks').execPopulate()
-  console.log(user.tasks)
-}
-
-main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -72,9 +72,8 @@ router.patch('/users/me', auth, async (req, res) => {
 
 router.delete('/users/me', auth, async (req, res) => {
   try {
-    // await req.user.remove()
-    await User.deleteOne({ _id: req.user._id })
-    res.send(req.user)
+    await req.user.deleteOne()
+    res.status(200).send(req.user)
   } catch (e) {
     res.status(500).send()
   }
