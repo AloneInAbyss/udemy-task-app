e5b2a129a8d9dfc2380a9afe34898061957d880a
Thiago Assi <thiago-assi@outlook.com>
2021-06-07 12:14:23 -0300

    delete avatar image

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Upload files
-const multer = require('multer')
-const upload = multer({
-  dest: 'images',
-  limits: {
-    fileSize: 10000000
-  }
-})
+// // Upload files
+// const multer = require('multer')
+// const upload = multer({
+//   dest: 'images',
+//   limits: {
+//     fileSize: 10000000
+//   }
+// })
 
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send()
-}, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
-})
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//   res.send()
+// }, (error, req, res, next) => {
+//   res.status(400).send({ error: error.message })
+// })
 
 app.use(express.json())
 app.use(userRouter)
