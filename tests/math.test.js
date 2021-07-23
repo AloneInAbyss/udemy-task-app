dee4c62be21468dcdbe1e0503fab7fad472a5aed
Thiago Assi <thiago-assi@outlook.com>
2021-07-23 08:50:48 -0300

    async jest testing

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math')
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius, add } = require('../src/math')
 
 test('Should calculate total with tip', () => {
   const total = calculateTip(10, .3)
@@ -19,4 +19,23 @@ test('Calculate temperature from celsius to fahrenheit', () => {
 })
 test('Calculate temperature from fahrenheit to celsius', () => {
   expect(fahrenheitToCelsius(32)).toBe(0)
+})
+
+// test('Async test demo', (done) => {
+//   setTimeout(() => {
+//     expect(1).toBe(2);
+//     done();
+//   }, 2000);
+// })
+
+test('Should add two numbers', (done) => {
+  add(2, 3).then((sum) => {
+    expect(sum).toBe(5);
+    done();
+  })
+})
+
+test('Should add two numbers async/await', async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
 })
