40d3ebb8d5d38003bf855d7ee5b499a38d9389a3
Thiago Assi <thiago-assi@outlook.com>
2021-07-26 14:53:45 -0300

    wrapping up user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -90,4 +90,36 @@ test('Should not delete account for unaunthenticated user', async () => {
     .expect(401)
 })
 
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/E7AOehtXsAQ5G52.jpg')
+    .expect(200)
+  const user = await User.findById(userOneId)
+  expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name:'Jess'
+    })
+    .expect(200)
+  const user = await User.findById(userOneId)
+  expect(user.name).toEqual('Jess')
+})
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: 'Philadelphia'
+    })
+    .expect(400)
+})
+
 afterAll(async () => { await mongoose.connection.close() })
