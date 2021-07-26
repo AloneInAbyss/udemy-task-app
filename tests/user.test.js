12127b0fe4cfaeea74fd079d2064834072031592
Thiago Assi <thiago-assi@outlook.com>
2021-07-26 15:11:30 -0300

    setup task test suite

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,24 +1,10 @@
-const request = require('supertest')
-const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
+const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const { setupDatabase, userOne, userOneId } = require('./fixtures/db')
 
-const userOneId = new mongoose.Types.ObjectId()
-const userOne = {
-  _id: userOneId,
-  name: 'Thiago',
-  email: 'th.assi@example.com',
-  password: '1what??',
-  tokens: [{
-    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-  }]
-}
-
-beforeEach(async () => {
-  await User.deleteMany()
-  await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test('Should signup a new user', async () => {
   const response = await request(app).post('/users').send({
